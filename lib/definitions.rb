7739d452bbeb99dd151a3cdcaa23c0e7dcea424a
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-08-14 11:29:00 -0700

    Add spec and code to display all definitions of words.

---
+++ lib/definitions.rb
@@ -0,0 +1,21 @@
+class Definitions
+
+  @@definition_list = []
+
+  define_method(:initialize) do |first_definition|
+    @first_definition = first_definition
+
+  end
+
+  define_method(:first_definition) do
+    @first_definition
+  end
+
+  define_singleton_method(:all) do
+    @@definition_list
+  end
+
+
+
+
+end
---
+++ spec/definitions_spec.rb
@@ -0,0 +1,24 @@
+require('rspec')
+require('definitions')
+
+describe(Definitions) do
+  describe('#first_definition') do
+    it('returns the primary definition for a word') do
+      test_definition = Definitions.new('A mountain is a large landform that stretches above the surrounding land in a limited area, usually in the form of a peak.')
+      expect(test_definition.first_definition()).to(eq('A mountain is a large landform that stretches above the surrounding land in a limited area, usually in the form of a peak.'))
+    end
+  end
+
+  # describe('#second_definition') do
+  #   it('returns the secondary definition for a word') do
+  #     test_definition_1 = Definitions.new('A mountain is generally steeper than a hill.')
+  #     expect(test_definition_1.second_definition()).to(eq('A mountain is generally steeper than a hill.'))
+  #   end
+  # end
+
+  describe('.all') do
+    it('returns all definitions for words') do
+      expect(Definitions.all()).to(eq([]))
+    end
+  end
+end

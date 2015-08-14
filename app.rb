a190fa3f6f19149afe778ced4c4319459f851ca6
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-08-14 13:15:21 -0700

    Remove unneeded files.

--- app.rb
+++ app.rb
@@ -8,3 +8,16 @@ get('/') do
   @list_of_words = Words.all()
   erb(:index)
 end
+
+get('/new_word') do
+  @words = Words.all()
+  erb(:new_word)
+end
+
+post('/new_word') do
+  new_word = Words.new(params.fetch('new_word'))
+  new_word.save()
+  @words = Words.all()
+  @list_of_words = Words.all()
+  erb(:index)
+end
